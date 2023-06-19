b0b4c7ebf5cbb40ce9b258e10e618057f8a09c7e
Cameron Huang <73293255+Cameron933@users.noreply.github.com>
2023-06-19 23:16:24 +1000

    section1

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -3,13 +3,16 @@
 
 import {useState} from 'react'
 
+// provided a default value for initialState
+// make sure it isn't undefined at anytime
 function Greeting({initialName = ''}) {
   // 💣 delete this variable declaration and replace it with a React.useState call
-  // const name = ''
   const [name, setName] = useState(initialName)
 
   function handleChange(event) {
     // 🐨 update the name here based on event.target.value
+    // reassigning variable will not trigger a rerender of our component:
+    // name = event.target.value
     setName(event.target.value)
   }
 
@@ -17,7 +20,7 @@ function Greeting({initialName = ''}) {
     <div>
       <form>
         <label htmlFor="name">Name: </label>
-        <input onChange={handleChange} id="name" />
+        <input onChange={handleChange} id="name" value={name} />
       </form>
       {name ? <strong>Hello {name}</strong> : 'Please type your name'}
     </div>
@@ -25,7 +28,7 @@ function Greeting({initialName = ''}) {
 }
 
 function App() {
-  return <Greeting />
+  return <Greeting initialName="Kody" />
 }
 
 export default App
